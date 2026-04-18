bcb5fad10458ba582f17d83adea791314f579e79
Juan <perezorozcojuanesteban@gmail.com>
2026-04-18 15:37:35 -0500

    Se crea componente base de courses

---
+++ src/pages/course-catalog/courses.jsx
@@ -0,0 +1,34 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import CourseCard from "../../components/course-card";
+import SearchBar from "../../components/search-bar";
+import CourseCounter from "../../components/course-counter";
+
+const Courses = () => {
+  const courses = []; // luego vendrá de API
+
+  return (
+    <div>
+      <Header />
+
+      <SearchBar searchTerm="" onSearch={() => {}} />
+
+      <CourseCounter total={courses.length} />
+
+      <div>
+        {courses.map((course, index) => (
+          <CourseCard
+            key={index}
+            title={course.title}
+            category={course.category}
+            price={course.price}
+          />
+        ))}
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Courses;
